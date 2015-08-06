e85d4c02acc7e7b4d3a5e258dad52f3cd821eda8
Dan Wahlin <dwahlin@xmlforasp.net>
2015-08-05 23:57:18 -0700

    Added a simple class that defines get/set property blocks.

--- dist/auto.js
+++ dist/auto.js
@@ -1,12 +1,12 @@
-"use strict";
+'use strict';
 
-Object.defineProperty(exports, "__esModule", {
+Object.defineProperty(exports, '__esModule', {
     value: true
 });
 
-var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
 
 var Auto = (function () {
     function Auto(data) {
@@ -19,14 +19,14 @@ var Auto = (function () {
     }
 
     _createClass(Auto, [{
-        key: "getTotal",
+        key: 'getTotal',
         value: function getTotal(taxRate) {
             return this.price + this.price * taxRate;
         }
     }, {
-        key: "getDetails",
+        key: 'getDetails',
         value: function getDetails() {
-            return this.year + " " + this.make + " " + this.model;
+            return this.year + ' ' + this.make + ' ' + this.model;
         }
     }]);
 
@@ -34,3 +34,42 @@ var Auto = (function () {
 })();
 
 exports.Auto = Auto;
+
+var AutoWithProperties = (function () {
+    function AutoWithProperties(data) {
+        _classCallCheck(this, AutoWithProperties);
+
+        this._make;
+        this._model;
+    }
+
+    _createClass(AutoWithProperties, [{
+        key: 'make',
+        get: function get() {
+            return this._make;
+        },
+        set: function set(val) {
+            if (val) {
+                this._make = val;
+            } else {
+                this._make = 'No Make';
+            }
+        }
+    }, {
+        key: 'model',
+        get: function get() {
+            return this._model;
+        },
+        set: function set(val) {
+            if (val) {
+                this._model = val;
+            } else {
+                this._model = 'No Model';
+            }
+        }
+    }]);
+
+    return AutoWithProperties;
+})();
+
+exports.AutoWithProperties = AutoWithProperties;
--- js/auto.js
+++ js/auto.js
@@ -13,4 +13,40 @@ export class Auto {
     getDetails() {
         return `${this.year} ${this.make} ${this.model}`;
     }
+    
 }
+
+export class AutoWithProperties {
+    constructor(data) {
+        this._make;
+        this._model;
+    }
+
+    get make() {
+        return this._make;
+    }
+    
+    set make(val) {
+        if (val) {
+            this._make = val;
+        } else {
+            this._make = 'No Make';
+        }
+    }
+    
+    get model() {
+        return this._model;
+    }
+    
+    set model(val) {
+        if (val) {
+            this._model = val;
+        } else {
+            this._model = 'No Model';
+        }
+        
+    }
+}
+
+
+
